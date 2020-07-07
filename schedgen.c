cbd8f63bd351f33c3b1aa649decc4baa8d7ceca5
dcerdt <67967297+dcerdt@users.noreply.github.com>
2020-07-07 18:38:27 -0500

    Update schedgen.c

--- schedgen.c
+++ schedgen.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
 
 	if(argc == 5){
 		if(atoi(argv[1]) > 0 && (atof(argv[2])>=0 && atof(argv[2])<1) && (atof(argv[3])>=0 && atof(argv[3])<1)){
-			printf("Por el momento todo bien");
+			printf("Por el momento todo bien / jajajajaajaj");
 		}else{
 		printf("ERROR: Valores incorrectos\n");
 		}
